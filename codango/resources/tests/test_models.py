912b99ac2021ae8ff07b5fbc542a3165af33c03b
IniOluwa Fageyinbo <inioluwafageyinbo@gmail.com>
2015-08-31 19:41:15 +0100

    [#102146168] Resources complete

--- codango/resources/tests/test_models.py
+++ codango/resources/tests/test_models.py
@@ -1,12 +1,7 @@
 #import unittest
 from django.test import TestCase
-#from resources import models
-from resources.models import Resources
+from resources.models import Resource
 
 class ModelsTestCase(TestCase):
 
-	def test_resource_text(self):
-		self.assertEqual(resource_text, models.CharField(max_length=1024))
-
-	# def test_check(self):
-	# 	self.assertEqual(1+3, 4)
+	
--- codango/resources/tests/test_views.py
+++ codango/resources/tests/test_views.py
@@ -2,8 +2,3 @@ import unittest
 from django.test import TestCase
 #from resources import views
 
-# class ViewsTestCase(TestCase):
-
-# 	def test_check(self):
-# 		self.assertEqual(True, True)
-
