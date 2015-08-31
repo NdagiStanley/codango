bd05e616ebefd64c37d63abea65a1034a21f0541
Jubril Issa <jubrilissa@gmail.com>
2015-08-31 15:35:30 +0100

    [#102150308] Feature: Create UserAccount model

--- codango/account/models.py
+++ codango/account/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
+
+
+class UserAccount(AbstractBaseUser):
+    username = models.CharField(max_length=100, unique=True)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+
