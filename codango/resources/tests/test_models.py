95a27f5a95ab6cc3452f963ac24263ea699e6c5d
IniOluwa Fageyinbo <inioluwafageyinbo@gmail.com>
2015-09-01 09:36:01 +0100

    [#102146168] models testing completed

--- codango/resources/tests/test_models.py
+++ codango/resources/tests/test_models.py
@@ -1,17 +1,15 @@
 from django.test import TestCase
 from resources.models import Resource
 from django.contrib.auth.models import User
-from django.test.client import Client
 from django.core.urlresolvers import reverse
 
 class ResourceTest(TestCase):
 
-	# def setUp(self):
-	# 	self.client = client()
-		#person = self.client.session['_auth_user_id']
+	def setUp(self):
+		self.user = User.objects.create(username='inioluwafageyinbo', password='codango')
 
-	def test_for_resources(self, title = "Some words", text = "some more words", author = '_auth_user_id'):
-		return Resource.objects.create(title = title, text = text, author = author)
+	def create_resources(self, title = "Some words", text = "some more words"):
+		return Resource.objects.create(title = title, text = text, author = self.user)
 
 	def test_for_resource_creation(self):
 		create = self.create_resources()
