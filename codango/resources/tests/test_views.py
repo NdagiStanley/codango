26b8f2bf4a357f2ac8d20122946263269b8a9a1d
andela-ifageyinbo <inioluwa.fageyinbo@andela.com>
2015-09-02 15:46:56 +0100

    Update test_views.py

--- codango/resources/tests/test_views.py
+++ codango/resources/tests/test_views.py
@@ -8,37 +8,11 @@ from django.core.urlresolvers import reverse
 class ResourceTestViews(TestCase):
 
 	def setUp(self): 
-		cli = Client()   
-		#response = cli.post('/login', {'username': 'inioluwa', 'password': 'codango'})
+		self.client = Client()   
 
 	def test_ListView(self):
 		url = reverse("resources.views.ListView")
-		resp = self.client.get(url)
+		response = self.client.get(url)
 		self.assertEqual(response.status_code, 200)
-		self.assertIn(create.title, resp.content)
+		self.assertIn(create.title, response.content)
 		
-
-	# def test_ListView(self):
-	# 	Resource.objects.create()
-	# 	view = ListView.as_view()
-	# 	response = view()
-	# 	self.assertEqual(response.status_code, 200)
-	# 	self.assertIn(create.title, response.content)
-
-
-	# def test_resources_views():
-	# 	# create = self.create_resource()
-	# 	url = reverse("resources.views.resources")
-
-	# 	self.assertEqual(resp.status_code, 200)
-	# 	self.assertIn(create.title, resp.content)
-
-	# def create_resources(self, title = "Some words", text = "some more words"):
-	# 	return Resource.objects.create(title = title, text = text, author = self.client)
-
-	# def test_ListView(self):
-	# 	c = self.create_resources()
-	# 	url = reverse("resources.views.resources")
-	# 	response = self.client.get(url)
-	# 	self.assertEqual(response.status_code, 200)
-	# 	self.assertIn(c.title, response.content)
