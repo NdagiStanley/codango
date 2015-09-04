11a82b3c1b80ae7bb0d539c39317983a6c696415
olufunmiladeo <olufunmilade.oshodi@andela.com>
2015-09-04 11:58:32 +0100

    [#102586948] tests

--- codango/account/tests/test_forms.py
+++ codango/account/tests/test_forms.py
@@ -1,1 +1,15 @@
-__author__ = 'Jubril'
+from django.test import TestCase
+
+from account.forms import LoginForm
+
+
+class LoginFormTest(TestCase):
+
+    def test_form_inputs(self):
+        form_data = {
+            'username': 'olufunmilade.oshodi@andela.com',
+            'password': 'password',
+        }
+
+        form = LoginForm(data=form_data)
+        self.assertTrue(form.is_valid())
--- codango/account/tests/test_views.py
+++ codango/account/tests/test_views.py
@@ -1,2 +1,42 @@
-from django.test import Client
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.core.urlresolvers import resolve
 
+
+class IndexViewTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        User.objects.create_user(
+            username='olufunmilade.oshodi@andela.com',
+            password='password'
+        )
+
+    def test_can_reach_index_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_right_view_for_index_is_returned(self):
+        match = resolve('/')
+        self.assertEqual(match.url_name, 'index')
+
+    def test_can_login(self):
+        response = self.client.post('/', {
+            'username': 'olufunmilade.oshodi@andela.com',
+            'password': 'password'
+        })
+        self.assertEqual(response.status_code, 302)
+
+
+class HomeViewTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_can_reach_home_page(self):
+        response = self.client.get('/home')
+        self.assertEqual(response.status_code, 301)
+
+    def test_right_view_for_home_is_returned(self):
+        match = resolve('/home/')
+        self.assertEqual(match.url_name, 'home')
