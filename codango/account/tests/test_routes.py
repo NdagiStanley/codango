2050a340696e78a7067694117a3f494c2e82fbc2
IniOluwa Fageyinbo <inioluwafageyinbo@gmail.com>
2015-09-05 14:44:30 +0100

    [#102586100] Feature-Forgot-Password Testing

--- codango/account/tests/test_routes.py
+++ codango/account/tests/test_routes.py
@@ -2,6 +2,9 @@ from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from django.core.urlresolvers import resolve
 
+from account.views import ForgotPassword, ResetPassword
+
+
 
 class IndexViewTest(TestCase):
 
@@ -40,3 +43,15 @@ class HomeViewTest(TestCase):
     def test_right_view_for_home_is_returned(self):
         match = resolve('/home/')
         self.assertEqual(match.url_name, 'home')
+
+class ForgotResetTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_forgot_route_resolves_to_correct_view(self):
+        response = self.client.get('/account/recovery/')
+        self.assertEqual(response.resolver_match.func.__name__, ForgotPassword.as_view().__name__)
+
+    def test_reset_route_resolves_to_correct_view(self):
+        response = self.client.get('/account/recovery/ajkzfYba9847DgJ7wbkwAaSbkTjUdawGG998qo3HG8qae83')
+        self.assertEqual(response.resolver_match.func.__name__, ResetPassword.as_view().__name__)
