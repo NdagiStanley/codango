afee9808d415e3b3637d15e9ab7de7117d6df7f1
andela-ifageyinbo <inioluwa.fageyinbo@andela.com>
2015-09-10 11:42:23 +0100

    Delete Forms.py
    Deleted Forms -- Conflicting with forms on local repository

--- codango/account/Forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-
-class ResetForm(forms.Form):
-    
-    password = forms.CharField(label='New Password', required=True, max_length=200, widget=forms.PasswordInput())
-
-    password_conf = forms.CharField(label='Confirm New Password', required=True, max_length=200, widget=forms.PasswordInput())
