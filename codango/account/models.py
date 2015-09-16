5e81f3a98284cec75488a2f00e45efe364b9b872
Jubril Issa <issa.jubril@andela.com>
2015-09-16 10:51:27 +0100

    [Feature #103187600] made changes to the User profile model

--- codango/account/models.py
+++ codango/account/models.py
@@ -1,16 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, User
 
 # Create your models here.
 
 
-class UserAccount(AbstractBaseUser):
+class UserProfile(models.Model):
     def get_short_name(self):
         pass
 
     def get_full_name(self):
         pass
 
+    user = models.OneToOneField(User)
     username = models.CharField(max_length=100, unique=True)
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
+
