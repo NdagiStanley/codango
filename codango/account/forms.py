96625f9baeb5d799365ebb82cbea4275b50a6e4c
olufunmiladeo <olufunmilade.oshodi@andela.com>
2015-09-18 16:32:50 +0100

    [#103187492] Added logout functionality

--- codango/account/forms.py
+++ codango/account/forms.py
@@ -15,39 +15,42 @@ class LoginForm(forms.Form):
 
     checkbox = forms.BooleanField(label='Remember Me', required=False)
 
-    
-class ResetForm(forms.Form):
-    
-    password = forms.CharField(label='New Password', required=True, max_length=200, widget=forms.PasswordInput(
-      attrs={
-        "placeholder": "Your New Password"
-      }
-    ))
-
-    password_conf = forms.CharField(label='Confirm New Password', required=True, max_length=200, widget=forms.PasswordInput(
-       attrs={
-        "placeholder": "Confirm Your New Password"
-      }
-    ))
-
 
 class RegisterForm(forms.Form):
     username = forms.CharField(max_length=300)
     email = forms.EmailField()
     password = forms.CharField(max_length=100, widget=forms.PasswordInput())
-    password_conf = forms.CharField(max_length=100, widget=forms.PasswordInput())
+    password_conf = forms.CharField(
+        max_length=100, widget=forms.PasswordInput())
+
+
+class ResetForm(forms.Form):
+
+    password = forms.CharField(label='New Password', required=True, max_length=200, widget=forms.PasswordInput(
+        attrs={
+            "placeholder": "Your New Password"
+        }
+    ))
+
+    password_conf = forms.CharField(label='Confirm New Password', required=True, max_length=200, widget=forms.PasswordInput(
+        attrs={
+            "placeholder": "Confirm Your New Password"
+        }
+    ))
 
     def clean_username(self):
         try:
             User.objects.get(username=self.cleaned_data['username'])
         except User.DoesNotExist:
             return self.cleaned_data['username']
-        raise forms.ValidationError("This user already exist in the database, please choose another username")
+        raise forms.ValidationError(
+            "This user already exist in the database, please choose another username")
 
     def clean(self):
         if 'password' in self.cleaned_data and 'password_conf' in self.cleaned_data:
             if self.cleaned_data['password'] != self.cleaned_data['password_conf']:
-                raise forms.ValidationError("You must type in the same password each time")
+                raise forms.ValidationError(
+                    "You must type in the same password each time")
         return self.cleaned_data
 
     def save(self):
