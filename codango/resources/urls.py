8c1497a522162733aaa81cc947c9a4a3f5650e20
IniOluwa Fageyinbo <inioluwafageyinbo@gmail.com>
2015-09-24 11:01:12 +0100

    Resource detail done

--- codango/resources/urls.py
+++ codango/resources/urls.py
@@ -2,22 +2,18 @@ from django.conf.urls import url
 from resources import views
 
 urlpatterns = [
-        url(r'^create', views.ResourceCreate.as_view(), name='resources_create'),
+    url(r'^create', views.ResourceCreate.as_view(), name='resources_create'),
 
-        url(r'^list', views.ResourceList.as_view(), name='resources_list'),
+    url(r'^list', views.ResourceList.as_view(), name='resources_list'),
 
-        url(r'^(?P<pk>[0-9]+)/$', views.ResourceDetail.as_view(), name='resources_detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.ResourceDetail.as_view(),
+        name='resources_detail'),
 
-        # url(r'^create/$',
-        #     views.ResourceCreate.as_view(),
-        #     name='resources_create'),
+    url(r'^(?P<pk>[0-9]+)/$', views.ResourceUpdate.as_view(),
+        name='resources_update'),
 
-        # url(r'^(?P<pk>[0-9]+)/update/$',
-        #     views.ResourceUpdate.as_view(),
-        #     name='resources_update'),
-
-        # url(r'^(?P<pk>[0-9]+)/delete/$',
-        #     views.ResourceDelete.as_view(),
-        #     name='resources_delete'),
+    # url(r'^(?P<pk>[0-9]+)/delete/$',
+    #     views.ResourceDelete.as_view(),
+    #     name='resources_delete'),
 
 ]
--- codango/resources/views.py
+++ codango/resources/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView, View
+from django.views.generic import TemplateView, UpdateView, DeleteView, View
 from django.core.urlresolvers import reverse_lazy
 from resources.models import Resource
 from resources.forms import ResourceForm
@@ -8,6 +8,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
 
 # Create your views here.
+
+
 class ResourceCreate(TemplateView):
     form_class = ResourceForm
     template_name = 'resources/create.html'
@@ -29,12 +31,34 @@ class ResourceCreate(TemplateView):
 
 
 class ResourceList(View):
+
     def get(self, request):
         resource_list = Resource.objects.all()
-        return render(request, 'resources/list.html',{'resource_list': resource_list})
+        return render(request, 'resources/list.html', {'resource_list': resource_list})
 
 
 class ResourceDetail(View):
+
     def get(self, request, *args, **kwargs):
-        resource_detail = Resource.objects.get(id = kwargs.get('pk'))
+        resource_detail = Resource.objects.get(id=kwargs.get('pk'))
         return render(request, 'resources/detail.html', {'resource_detail': resource_detail})
+
+
+class ResourceUpdate(ResourceCreate):
+
+    def get(self, request, *args, **kwargs):
+        resource_update = Resource.get.object()
+        context = self.get_context_data().resource_update()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST, request.FILES)
+        form.author = request.user
+        if form.is_valid():
+            resource = form.save(commit=False)
+            resource.save()
+            return HttpResponseRedirect('resources/update')
+
+
+class ResourceDelete(View):
+    pass
