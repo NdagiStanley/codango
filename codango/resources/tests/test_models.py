5b1ee4cc5b021c965ec447c857b9081a090fcd89
IniOluwa Fageyinbo <inioluwafageyinbo@gmail.com>
2015-10-02 13:42:53 +0100

    (fixes) Failing tests fixed

--- codango/resources/tests/test_models.py
+++ codango/resources/tests/test_models.py
@@ -7,12 +7,12 @@ from django.core.urlresolvers import reverse
 class ResourceTestModels(TestCase):
 
     def setUp(self):
-        self.user = User.objects.create(username='inioluwafageyinbo', password='codango')
+        self.user = User.objects.create(
+            username='inioluwafageyinbo', password='codango')
 
-    def create_resources(self, title = "Some words", text = "some more words"):
-        return Resource.objects.create(title = title, text = text, author = self.user)
+    def create_resources(self, text='some more words', resource_file='resource_file'):
+        return Resource.objects.create(text=text, author=self.user, resource_file=resource_file)
 
     def test_for_resource_creation(self):
         create = self.create_resources()
         self.assertTrue(isinstance(create, Resource))
-        self.assertEqual(create.__str__(), create.title)
