8fc39d48e67df812f855fe6e9af337a1ebf19024
IniOluwa Fageyinbo <inioluwafageyinbo@gmail.com>
2015-10-02 13:52:59 +0100

    (fixes) Conflicting files removed.

--- codango/account/static/js/app.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-	$('#id_pdf_button').on('click', function(eve){
-		eve.preventDefault();
-		$('#id_pdf_file').toggleClass('show');
-	});
-});
--- codango/resources/uploader.py
+++
@@ -1,6 +0,0 @@
-import cloudinary
-
-def upload_resource(file, **options):
-    result = upload(file, **options)
-    return cloudinary.CloudinaryResource(result["public_id"], version=str(result["version"]),
-        format=result.get("format"), type=result["type"], resource_type=result["resource_type"], metadata=result)
