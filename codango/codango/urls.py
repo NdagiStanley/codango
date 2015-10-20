707a41f75d45b6e8d47e2987cf3f35b4ad69cc97
IniOluwa Fageyinbo <inioluwafageyinbo@gmail.com>
2015-10-20 16:34:24 +0100

    (fix) Issue with the tests

--- codango/codango/urls.py
+++ codango/codango/urls.py
@@ -22,7 +22,6 @@ import account.urls
 
 urlpatterns = [
     url(r'^', include(account.urls)),
-    url(r'^resources/', include(resources.urls)),
     url(r'^admin/', include(admin.site.urls)),
 
 ]
