f23291d0471b9adfe5b75c0a6cb06f950d20b145
olufunmiladeo <olufunmilade.oshodi@andela.com>
2015-10-20 16:24:47 +0100

    (fixes) Allow heroku to import base.py settings on production

--- codango/codango/settings/__init__.py
+++ codango/codango/settings/__init__.py
@@ -3,3 +3,6 @@ if not os.getenv('CI') and not os.getenv('HEROKU'):
     from django_envie.workroom import convertfiletovars
     convertfiletovars()
     from development import *
+
+if os.getenv('HEROKU') is not None:
+    from production import *
