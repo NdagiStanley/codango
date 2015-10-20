0781baa2b12ad00cba398f4fb87b1c8682bf647a
IniOluwa Fageyinbo <inioluwafageyinbo@gmail.com>
2015-10-20 16:37:34 +0100

    (fixes) Issue with the tests

--- codango/codango/urls.py
+++ codango/codango/urls.py
@@ -17,7 +17,6 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
-import resources.urls
 import account.urls
 
 urlpatterns = [
