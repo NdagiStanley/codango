243ecd014864e2ecb31cf18bb6911429d131efa8
olufunmiladeo <tyos137@gmail.com>
2015-11-12 07:34:42 +0100

    [Test #107706824] wrote mock test to mock the return value of a cloudinary pre_save method

--- codango/resources/tests/test_models.py
+++ codango/resources/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from resources.models import Resource
 from django.contrib.auth.models import User
+from mock import patch
 
 
 class ResourceTestModels(TestCase):
@@ -8,10 +9,19 @@ class ResourceTestModels(TestCase):
     def setUp(self):
         self.user = User.objects.create(
             username='inioluwafageyinbo', password='codango')
-
-    def create_resources(self, text='some more words', resource_file='resource_file'):
-        return Resource.objects.create(text=text, author=self.user, resource_file=resource_file)
+        self.resource = Resource.objects.create(
+            text='test file',
+            author=self.user
+        )
 
     def test_for_resource_creation(self):
-        create = self.create_resources()
-        self.assertTrue(isinstance(create, Resource))
+        self.assertTrue(Resource.objects.all())
+
+    def test_file_save(self):
+        with patch.object(Resource.resource_file.field, 'pre_save', return_value='test.pdf') as mock_method:
+            uploadlink = mock_method('test.pdf')
+            self.resource.resource_file = uploadlink
+            self.resource.save()
+            publicid = Resource.objects.filter(
+                resource_file=uploadlink)[0].resource_file.public_id
+            self.assertEqual(publicid, 'test')
