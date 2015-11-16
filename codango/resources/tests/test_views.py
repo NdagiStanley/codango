52611fb8ef87972d38aa070aa69ddfd9750a4d89
Abiodun Shuaib <abiodun.shauib.m@gmail.com>
2015-11-16 03:12:24 +0100

    [TEST #107707062] votes views  testcases

--- codango/resources/tests/test_views.py
+++ codango/resources/tests/test_views.py
@@ -1,1 +1,72 @@
 from django.test.utils import setup_test_environment
+setup_test_environment()
+from django.test import Client, TestCase
+import json
+from django.contrib.auth.models import User
+from django.core.urlresolvers import resolve, reverse
+
+from resources.models import Resource
+from votes.models import Vote
+
+
+
+class AjaxCommunityView(TestCase):
+
+	def setUp(self):
+		self.client = Client()
+		self.user = User.objects.create(username='Abiodun', password='shuaib')
+		self.user.set_password('shuaib')
+		self.user.save()
+		self.login = self.client.login(username='Abiodun', password='shuaib')
+
+	def create_resources(self, text='some more words', resource_file='resource_file'):
+		return Resource.objects.create(id=100, text=text, author=self.user, resource_file=resource_file)
+
+
+	def test_can_reach_ajax_community_page(self):
+		self.assertTrue(self.login)
+		response = self.client.get(reverse('community',args=('all',)),content_type='application/json',
+                HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+		self.assertTrue(response.status_code == 200)
+		self.assertContains(response, "There are currently no posts")
+	def test_can_post_new_ajax_content(self):
+		self.assertTrue(self.login)
+		response = self.client.post('/resource/newresource',
+                {'text': '1',},
+                              HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+		print response
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, "success")
+
+	def test_user_can_upvote(self):
+		self.assertTrue(self.login)
+		resource = self.create_resources()
+		response = self.client.post('/resource/100/like',{'resource_id':100},
+                              HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+		self.assertEqual(response.status_code,200)
+		self.assertEqual(len(resource.upvotes()),1)
+	def test_user_can_downvote(self):
+		self.assertTrue(self.login)
+		resource = self.create_resources()
+		response = self.client.post('/resource/100/unlike',{'resource_id':100},
+                              HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+		self.assertEqual(response.status_code,200)
+		self.assertEqual(len(resource.downvotes()),1)
+	def test_user_can_get_persisten_vote(self):
+		self.assertTrue(self.login)
+		resource = self.create_resources()
+		response = self.client.post('/resource/100/unlike',{'resource_id':100},
+                              HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+		response = self.client.post('/resource/100/like',{'resource_id':100},
+                              HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+		self.assertEqual(response.status_code,200)
+		self.assertEqual(len(resource.upvotes()),1)
+	def test_user_can_delete_vote(self):
+		self.assertTrue(self.login)
+		resource = self.create_resources()
+		response = self.client.post('/resource/100/unlike',{'resource_id':100},
+                              HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+		response = self.client.post('/resource/100/unlike',{'resource_id':100},
+                              HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+		self.assertEqual(response.status_code,200)
+		self.assertEqual(len(resource.upvotes()),0)
--- codango/resources/urls.py
+++ codango/resources/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from resources import views
 
 urlpatterns = [
-url(r'^/(?P<resource_id>[0-9]+)/(?P<action>\w+)$', views.VoteAjax.as_view(), name='vote'),
-url(r'^/newresource$', views.AjaxCommunityView.as_view(), name='newresource'),
+url(r'^(?P<resource_id>[0-9]+)/(?P<action>\w+)$', views.VoteAjax.as_view(), name='vote'),
+url(r'^newresource$', views.AjaxCommunityView.as_view(), name='newresource'),
 url(r'^ajax/community/(?P<community>\w+|)$',views.AjaxCommunityView.as_view(), name='community'),
 ]
 
