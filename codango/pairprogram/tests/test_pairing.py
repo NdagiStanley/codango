330d01ab50dfa2219a318df54a5b4172c4b09250
Issa Jubril <issa.jubril@andela.com>
2015-11-30 11:32:47 +0100

    [Feature #108852726] created basic test template

---
+++ codango/pairprogram/tests/test_pairing.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+
+
+class PairTestCase(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_user_can_invite_another_user(self):
+        pass
+
+    def test_user_can_initiate_a_pairing_session(self):
+        pass
+
+    def test_user_can_join_a_pairing_session(self):
+        pass
+
+    def test_pairing_participants_can_both_code(self):
+        pass
