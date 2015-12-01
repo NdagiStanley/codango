a6dd426f2c0acf3566e4e48f3a48bbbf60058518
Issa Jubril <issa.jubril@andela.com>
2015-12-01 16:28:00 +0100

    [Feature #108852726] work in progress on initiating a pair programming session

--- codango/pairprogram/views.py
+++ codango/pairprogram/views.py
@@ -2,16 +2,21 @@ from django.shortcuts import render
 from django.views.generic import View, TemplateView
 from django.http import HttpResponse, Http404
 from pairprogram.models import PairProgram
+from account.views import LoginRequiredMixin
+from userprofile.models import UserProfile, Follow
 
 # Create your views here.
 
 
-class PairView(TemplateView):
+class PairView(LoginRequiredMixin, TemplateView):
 
     template_name = 'pairprogram/pair.html'
 
     def get_context_data(self, **kwargs):
         context = super(PairView, self).get_context_data(**kwargs)
+        context['friendlists'] = UserProfile.objects.all()
+
+        return context
 
     def post(self, request, **kwargs):
 
---
+++ codango/static/js/pair.js
@@ -0,0 +1,62 @@
+function sendSession(key){
+    var ajaxinfo = {
+        url: '/pair/',
+        data:{
+            sessionKey: key
+        },
+        type: "POST",
+        success:function(data){
+            console.log(data);
+        },
+        error: function(xhr){
+            console.log(xhr.responseText);
+        }
+    };
+    $.ajax(ajaxinfo);
+}
+
+function init(language, theme) {
+      //// Initialize Firebase.
+      var firepadRef = getExampleRef();
+    var sessionKey = firepadRef.key();
+    sendSession(sessionKey);
+      // TODO: Replace above line with:
+      // var firepadRef = new Firebase('<YOUR FIREBASE URL>');
+      //// Create ACE
+      var editor = ace.edit("firepad-container");
+      editor.setTheme("ace/theme/"+ theme);
+      var session = editor.getSession();
+      session.setUseWrapMode(true);
+      session.setUseWorker(false);
+      session.setMode("ace/mode/"+ language);
+      console.log(language)
+      console.log(theme)
+      //// Create Firepad.
+      var firepad = Firepad.fromACE(firepadRef, editor, {
+        defaultText: '// JavaScript Editing with Firepad!\nfunction go() {\n  var message = "Hello, world.";\n  console.log(message);\n}'
+      });
+    }
+    // Helper to get hash from end of URL or generate a random one.
+function getExampleRef() {
+      var ref = new Firebase('https://firepad.firebaseio-demo.com');
+      var hash = window.location.hash.replace(/#/g, '');
+      if (hash) {
+        ref = ref.child(hash);
+      } else {
+        ref = ref.push(); // generate unique location.
+        window.location = window.location + '#' + ref.key(); // add it as a hash to the URL.
+      }
+      if (typeof console !== 'undefined')
+        console.log('Firebase data: ', ref.toString());
+      return ref;
+    }
+
+$(document).ready(function() {
+    $('#language').change(function (e) {
+        init($(this).val());
+    });
+    $('#theme').change(function (e) {
+        init($(this).val());
+    });
+    window.onload = init('javascript', 'monokai');
+});
