18ef9e23f2fc7f4c5443a3efb41ea5390dd597fc
Abiodun Shuaib <abiodun.shauib.m@gmail.com>
2015-12-11 21:34:24 +0100

    [Test #108761804] Test Implementations for user activity model and views

--- codango/comments/tests/test_views.py
+++ codango/comments/tests/test_views.py
@@ -6,7 +6,6 @@ from comments.models import Comment
 from django.test.utils import setup_test_environment
 setup_test_environment()
 
-
 class CommentActionTest(TestCase):
 
     def setUp(self):
@@ -16,38 +15,27 @@ class CommentActionTest(TestCase):
         self.user.save()
         self.login = self.client.login(username='Abiodun', password='shuaib')
 
-    def create_resources(
-            self, text='some more words',
-            resource_file='resource_file'):
-        return Resource.objects.create(
-            id=100, text=text, author=self.user,
-            resource_file=resource_file
-        )
+    def create_resources(self, text='some more words', resource_file='resource_file'):
+        return Resource.objects.create(id=100, text=text, author=self.user, resource_file=resource_file)
 
     def test_user_can_post_new_comments(self):
         self.assertTrue(self.login)
-        self.create_resources()
-        response = self.client.post(
-            '/comment/',
-            {'content': 'test_content',
-             'resource_id': 100},
-            HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        resource = self.create_resources()
+        response = self.client.post('/comment/',
+                                    {'content': 'test_content',
+                                        'resource_id': 100},
+                                    HTTP_X_REQUESTED_WITH='XMLHttpRequest')
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "success")
+        self.assertContains(response, 'status')
 
     def test_user_can_delete_comments(self):
         self.assertTrue(self.login)
         resource = self.create_resources()
         comment = Comment(
-            id=200,
-            author=self.user,
-            resource=resource,
-            content="Test comment"
-        )
+            id=200, author=self.user, resource=resource, content="Test comment")
         comment.save()
-        response = self.client.delete(
-            '/comment/200',
-            HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        response = self.client.delete('/comment/200',
+                                      HTTP_X_REQUESTED_WITH='XMLHttpRequest')
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "success")
 
@@ -55,15 +43,10 @@ class CommentActionTest(TestCase):
         self.assertTrue(self.login)
         resource = self.create_resources()
         comment = Comment(
-            id=200,
-            author=self.user,
-            resource=resource,
-            content="Test comment"
-        )
+            id=200, author=self.user, resource=resource, content="Test comment")
         comment.save()
         json_data = json.dumps({'content': 'Another Content', })
-        response = self.client.put(
-            '/comment/200', json_data, content_type='application/json',
-            HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        response = self.client.put('/comment/200', json_data, content_type='application/json',
+                                   HTTP_X_REQUESTED_WITH='XMLHttpRequest')
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "success")
