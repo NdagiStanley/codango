92aa29ea46215cb855984822697fc94886974b02
Abiodun Shuaib <abiodun.shauib.m@gmail.com>
2015-12-16 09:55:04 +0100

    [Fix #108761804] Hot fix

--- codango/resources/urls.py
+++ codango/resources/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from resources import views
 
 urlpatterns = [
-    url(r'^(?P<resource_id>[0-9]+)/(?P<action>like|unlike)$',
+    url(r'^(?P<resource_id>[0-9]+)/(?P<action>likes|unlikes)$',
         views.ResourceVoteView.as_view(), name='resource_vote'),
     url(r'^create$', views.CommunityView.as_view(), name='resource_create'),
     url(r'^ajax/community/(?P<community>\w+|)$',
