71edf9318be54768829a12e9d6635dc8ca2361c4
Issa Jubril <issa.jubril@andela.com>
2015-12-18 12:02:28 +0100

    [Feature #107707920] Modfied pair programming models

--- codango/pairprogram/models.py
+++ codango/pairprogram/models.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 
 class Session(models.Model):
     session_name = models.CharField(max_length=200, null=True)
-    last_active_date = models.DateTimeField(default=timezone.now())
+    last_active_date = models.DateTimeField(default=timezone.now)
     status = models.BooleanField(default=True)
     initiator = models.ForeignKey(User)
 
@@ -15,4 +15,4 @@ class Session(models.Model):
 class Participant(models.Model):
     participant = models.ForeignKey(User)
     session = models.ForeignKey(Session)
-    joined_date = models.DateTimeField(default=timezone.now())
+    joined_date = models.DateTimeField(default=timezone.now)
