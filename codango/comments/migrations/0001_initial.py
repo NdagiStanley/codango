4184ee9e6d6104af0c4061e78c5cfadb29329b1a
Chidiebere Nnadi <chidiebere.nnadi@gmail.com>
2016-03-08 13:19:07 +0300

    [Fix] Trying to get develop updated

--- codango/comments/migrations/0001_initial.py
+++ codango/comments/migrations/0001_initial.py
@@ -8,8 +8,8 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('resources', '0001_initial'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
@@ -18,7 +18,8 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('content', models.TextField()),
-                ('date_created', models.DateTimeField(auto_now=True)),
+                ('date_created', models.DateTimeField(auto_now_add=True)),
+                ('date_modified', models.DateTimeField(auto_now=True)),
                 ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
                 ('resource', models.ForeignKey(related_name='comments', to='resources.Resource')),
             ],
