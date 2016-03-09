4e43eee105b4e3dcab79cdf232db0ec8f955f221
Stanley Ndagi <stanley.ndagi@andela.com>
2016-03-09 09:59:48 +0300

    [Feature #111504508] Clean code

--- codango/resources/tests/test_api.py
+++ codango/resources/tests/test_api.py
@@ -4,10 +4,13 @@ from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
 from ..models import Resource
 
+# DRY variables to be used repeatedly
 user = {'username': 'stanmd', 'email': 'ndagi@gmail.com', 'password': '1234'}
 message = {"detail":
            "Authentication credentials were not provided."}
-
+url = '/api/v1/resources/'
+url_for_one = '/api/v1/resources/5'
+not_found_msg = {"detail": "Not found."}
 
 class ResourceTest(APITestCase):
     """Test /api/v1/resources/ endpoint"""
@@ -24,7 +27,6 @@ class ResourceTest(APITestCase):
     def test_C_resource(self):
         """Test Create resource"""
 
-        url = '/api/v1/resources/'
         entry = {'author': 1, 'text': "abcdefgh", 'language_tags': "PYTHON"}
         plain_response = self.client.post(url, entry)
         self.assertEqual(plain_response.data, message)
@@ -33,17 +35,16 @@ class ResourceTest(APITestCase):
 
         # set authentication token in header
         self.client.credentials(HTTP_AUTHORIZATION=self.token)
-        # Get to the resources url
         auth_response = self.client.post(url, entry)
+
         self.assertEqual(auth_response.data.get('text'), 'abcdefgh')
         self.assertEqual(auth_response.status_code, 201)
-        self.assertNotEqual(plain_response.data, {})
+        self.assertNotEqual(auth_response.data, {})
         self.assertEqual(Resource.objects.count(), 1)
 
     def test_R_resource(self):
         """Test Retrieve resources"""
 
-        url = '/api/v1/resources/'
         plain_response = self.client.get(url)
         self.assertEqual(plain_response.data, message)
         self.assertNotEqual(plain_response.data, {})
@@ -51,20 +52,25 @@ class ResourceTest(APITestCase):
 
         # set authentication token in header
         self.client.credentials(HTTP_AUTHORIZATION=self.token)
-        # Get to the resources url
         auth_response = self.client.get(url)
+
         self.assertEqual(auth_response.data['results'], [])
         self.assertEqual(auth_response.status_code, 200)
-        self.assertNotEqual(plain_response.data, {})
+        self.assertNotEqual(auth_response.data, {})
 
+    def test_pagination_resource(self):
         """Test pagination"""
+
+        # set authentication token in header
+        self.client.credentials(HTTP_AUTHORIZATION=self.token)
         # Create several resources
         for i in range(10):
             entry = {'author': User.objects.get().id,
-                     'text': "Resource%d" %i,
+                     'text': "Resource%d" % i,
                      'language_tags': "PYTHON"}
             self.client.post(url, entry)
         auth_response = self.client.get(url)
+
         # According to the number specified in the settings/base.py
         self.assertEqual(auth_response.data['count'], 10)
         self.assertEqual(auth_response.data['next'],
@@ -73,7 +79,7 @@ class ResourceTest(APITestCase):
         self.assertEqual(auth_response.data['previous'], None)
         self.assertEqual(len(auth_response.data['results']), 3)
         self.assertEqual(auth_response.status_code, 200)
-        self.assertNotEqual(plain_response.data, {})
+        self.assertNotEqual(auth_response.data, {})
         next_page_url = '/api/v1/resources/?page=2'
         self.assertEqual(self.client.get(next_page_url).data['next'],
                          "http://testserver/api/v1/resources/?page=3")
@@ -83,53 +89,52 @@ class ResourceTest(APITestCase):
 
     def test_R_specific_resource(self):
         """Test Retrieve specific resource"""
-        plain_response = self.client.get('/api/v1/resources/1')
+        plain_response = self.client.get(url_for_one)
         self.assertEqual(plain_response.data, message)
         self.assertNotEqual(plain_response.data, {})
         self.assertEqual(plain_response.status_code, 401)
 
         # set authentication token in header
         self.client.credentials(HTTP_AUTHORIZATION=self.token)
-        not_found_msg = {"detail": "Not found."}
-        # Get to the specific resources url
-        auth_response = self.client.get('/api/v1/resources/1')
+        auth_response = self.client.get(url_for_one)
+
         self.assertEqual(auth_response.data, not_found_msg)
         self.assertEqual(auth_response.status_code, 404)
-        self.assertNotEqual(plain_response.data, {})
+        self.assertNotEqual(auth_response.data, {})
 
         # Create several resources
         for i in range(10):
             entry = {'author': User.objects.get().id,
                      'text': "Resource%d" %i,
                      'language_tags': "PYTHON"}
-            self.client.post('/api/v1/resources/', entry)
-        auth_response = self.client.get('/api/v1/resources/5')
+            self.client.post(url, entry)
+        auth_response = self.client.get(url_for_one)
         self.assertNotEqual(auth_response.data, not_found_msg)
         self.assertEqual(auth_response.status_code, 200)
         self.assertEqual(auth_response.data.get('text'), 'Resource4')
 
     def test_U_specific_resource(self):
         """Test Update specific resource"""
+
         update_info = {"author": 1, "text": "Test Update",
                        "language_tags": "PYTHON", "resource_file": 'None',
                        "resource_file_name": 'None', "resource_file_size": 0,
                        "snippet_text": "random Text",
                        "date_added": "2016-03-01T13:54:52.326929Z",
                        "date_modified": "2016-03-01T13:54:52.326965Z"}
-        plain_response = self.client.put('/api/v1/resources/1', update_info)
+        plain_response = self.client.put(url_for_one, update_info)
         self.assertEqual(plain_response.data, message)
         self.assertNotEqual(plain_response.data, {})
         self.assertEqual(plain_response.status_code, 401)
 
         # set authentication token in header
         self.client.credentials(HTTP_AUTHORIZATION=self.token)
-
-        not_found_msg = {"detail": "Not found."}
         # Get to the specific resources url
-        auth_response = self.client.get('/api/v1/resources/1')
+        auth_response = self.client.get(url_for_one)
+
         self.assertEqual(auth_response.data, not_found_msg)
         self.assertEqual(auth_response.status_code, 404)
-        self.assertNotEqual(plain_response.data, {})
+        self.assertNotEqual(auth_response.data, {})
 
         # Create several resources
         for i in range(10):
@@ -137,32 +142,35 @@ class ResourceTest(APITestCase):
                      'text': "Resource%d" % i,
                      'language_tags': "PYTHON"}
             self.client.post('/api/v1/resources/', entry)
-        auth_response = self.client.put('/api/v1/resources/5', update_info)
+        auth_response = self.client.put(url_for_one, update_info)
         self.assertNotEqual(auth_response.data, not_found_msg)
         self.assertEqual(auth_response.status_code, 200)
         self.assertNotEqual(auth_response.data.get('text'), 'Resource4')
         self.assertEqual(auth_response.data.get('text'), 'Test Update')
 
     def test_D_specific_resource(self):
-        """Test Update specific resource"""
-        plain_response = self.client.delete('/api/v1/resources/1')
+        """Test Delete specific resource"""
+
+        plain_response = self.client.delete(url_for_one)
         self.assertEqual(plain_response.data, message)
         self.assertNotEqual(plain_response.data, {})
         self.assertEqual(plain_response.status_code, 401)
 
         # set authentication token in header
         self.client.credentials(HTTP_AUTHORIZATION=self.token)
-        not_found_msg = {"detail": "Not found."}
-        # Get to the specific resources url
-        auth_response = self.client.get('/api/v1/resources/1')
+        auth_response = self.client.get(url_for_one)
+
         self.assertEqual(auth_response.data, not_found_msg)
         self.assertEqual(auth_response.status_code, 404)
-        self.assertNotEqual(plain_response.data, {})
+        self.assertNotEqual(auth_response.data, {})
 
         # Create several resources
         for i in range(10):
-            entry = {'author': User.objects.get().id, 'text': "Resource%d" %i, 'language_tags': "PYTHON"}
+            entry = {'author': User.objects.get().id,
+                     'text': "Resource%d" % i,
+                     'language_tags': "PYTHON"}
             self.client.post('/api/v1/resources/', entry)
-        auth_response = self.client.delete('/api/v1/resources/5')
+        auth_response = self.client.delete(url_for_one)
         self.assertEqual(auth_response.data, None)
+        self.assertEqual(self.client.get(url).data['count'], 9)
         self.assertEqual(auth_response.status_code, 204)
