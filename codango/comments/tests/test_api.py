aab094f47545d9e155ebddeefdbdf4edcc557f3a
andela-jngatia <joan.ngatia@andela.com>
2016-03-09 10:23:54 +0300

    [Feature #111504778] Add tests for comment creation and retrieval.

--- codango/comments/tests/test_api.py
+++ codango/comments/tests/test_api.py
@@ -1,24 +1,64 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+from django.contrib.auth.models import User
+from resources.models import Resource
+
 message = {"detail":
            "Authentication credentials were not provided."}
+user = {'username': 'sterling',
+        'email': 'archer@yahoo.com', 'password': '1234'}
+
+url = '/api/v1/comments/'
 
 
 class CommentTest(APITestCase):
-    """Test /api/v1/comments/ endpoint"""
-    def test_comment(self):
-        response = self.client.get('/api/v1/comments/')
+    """Test /api/v1/comments/ endpoint."""
+
+    def setUp(self):
+        """Set up base details for comments endpoint."""
+        # register and login a dummy user
+        self.client.post('/api/v1/auth/register/', user, format='json')
+        auth_user = {'username': User.objects.get().username,
+                     'password': user['password']}
+        login_response = self.client.post('/api/v1/auth/login/', auth_user)
+        self.token = 'JWT ' + login_response.data.get('token')
+
+        # create a dummy resource
+        self.client.credentials(HTTP_AUTHORIZATION=self.token)
+        resource = {'author': 1, 'text': 'some random post'}
+        self.client.post('/api/v1/resources/', resource)
+
+        # create a dummy comment
+        self.comment = {'author': User.objects.get().id,
+                        'content': 'finally here',
+                        'resource': Resource.objects.get().id}
+
+    def test_comment_retrieval(self):
+        """Test for comment retrieval."""
+        # unsuccessful comment retrieval without authentication.
+        self.client.credentials(HTTP_AUTHORIZATION=' ')
+        response = self.client.get(url)
         self.assertEqual(response.data, message)
         self.assertNotEqual(response.data, {})
         self.assertEqual(response.status_code, 401)
 
-    """
-    Will be included in the CRUD of endpoints upon authentication
+        # successful comment retrieval with authentication.
+        self.client.credentials(HTTP_AUTHORIZATION=self.token)
+        auth_response = self.client.get(url)
+        self.assertEqual(auth_response.status_code, 200)
 
-    TO TEST AUTH PER URL
-    token = 'JWT ' + login_response.data.get('token')
-    # set authentication token in header
-    self.client.credentials(HTTP_AUTHORIZATION=token)
-    auth_response = self.client.post('/api/v1/....')
-    """
+    def test_comment_creation(self):
+        """Test for comment creation."""
+        # unsuccessful comment creation without authentication.
+        self.client.credentials(HTTP_AUTHORIZATION=' ')
+        response = self.client.post(url)
+        self.assertEqual(response.data, message)
+        self.assertNotEqual(response.data, {})
+        self.assertEqual(response.status_code, 401)
+
+        # successful comment creation with authentication.
+        self.client.credentials(HTTP_AUTHORIZATION=self.token)
+        auth_response = self.client.post(url, self.comment)
+        self.assertEqual(auth_response.status_code, 201)
+
