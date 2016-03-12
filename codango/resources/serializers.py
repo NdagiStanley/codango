eb418b3fd76d1905cedfe384ed649aa8f318a5b4
andela-jngatia <joan.ngatia@andela.com>
2016-03-13 01:08:11 +0300

    (Feature #111504778) Refactor resource serializer class to display comments.

--- codango/resources/serializers.py
+++ codango/resources/serializers.py
@@ -1,15 +1,20 @@
 from rest_framework import serializers
 
 from models import Resource
+from comments.serializers import CommentSerializer
 
 
 class ResourceSerializer(serializers.ModelSerializer):
     """Resource Serializer"""
 
+    comments = CommentSerializer(many=True)
+    author = serializers.ReadOnlyField(source='user.username')
+
+
     class Meta:
         model = Resource
         fields = ('author', 'text', 'language_tags', 'resource_file',
                   'resource_file_name', 'resource_file_size', 'snippet_text',
-                  'date_added', 'date_modified')
+                  'date_added', 'date_modified', 'comments')
 
         read_only_fields = ('date_modified', 'date_added')
