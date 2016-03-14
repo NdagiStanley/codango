bbb96b02bae5307d3bc1d304581e383d11269de1
andela-jngatia <joan.ngatia@andela.com>
2016-03-14 18:12:59 +0300

    [Chore #111504778] Code cleanup

--- codango/resources/api.py
+++ codango/resources/api.py
@@ -2,16 +2,16 @@ from rest_framework import generics, permissions
 from serializers import ResourceSerializer
 from models import Resource
 
-from rest_framework import generics
-
 
 class ResourceListAPIView(generics.ListCreateAPIView):
-    """For /api/v1/resources/ url path"""
+    """For /api/v1/resources/ url path."""
+
     queryset = Resource.objects.all()
     serializer_class = ResourceSerializer
 
 
 class ResourceDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
-    """For /api/v1/resources/<> url path"""
+    """For /api/v1/resources/<resource_id> url path."""
+
     queryset = Resource.objects.all()
     serializer_class = ResourceSerializer
