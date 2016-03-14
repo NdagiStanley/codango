e5f70a265563529bb0c4a2c48d08248eb47b778b
andela-jngatia <joan.ngatia@andela.com>
2016-03-14 18:11:41 +0300

    [Chore #111504778] Add tests for comments CRUD operations

--- codango/comments/tests/test_api.py
+++ codango/comments/tests/test_api.py
@@ -1,119 +1,115 @@
-from rest_framework import status
 from rest_framework.test import APITestCase
 
 from django.contrib.auth.models import User
-from resources.models import Resource
 from comments.models import Comment
 
+
+# DRY variables to be used repeatedly
 message = {"detail":
            "Authentication credentials were not provided."}
-user = {'username': 'sterling',
-        'email': 'archer@yahoo.com', 'password': '1234'}
+url = '/api/v1/resources/1/comments/'
+url2 = '/api/v1/resources/1/comments/2/'
+url3 = '/api/v1/resources/2/comments/'
+url4 = '/api/v1/resources/2/comments/2/'
+not_found_msg = {"detail": "Not found."}
 
-url = '/api/v1/comments/'
 
+class CommentsTest(APITestCase):
+    """Test /api/v1/resources/<resource_id>/comments/ endpoint"""
 
-class CommentTest(APITestCase):
-    """Test /api/v1/comments/ endpoint."""
+    # Include 3 comments ,one user and one resource for testing purposes
+    fixtures = ['comments.json']
 
     def setUp(self):
-        """Set up base details for comments endpoint."""
-        # register and login a dummy user
-        self.client.post('/api/v1/auth/register/', user, format='json')
+        """Set up base user and details for test running."""
+        # Login the user
         auth_user = {'username': User.objects.get().username,
-                     'password': user['password']}
+                     'password': 'ashley'}
         login_response = self.client.post('/api/v1/auth/login/', auth_user)
         self.token = 'JWT ' + login_response.data.get('token')
 
-        # create a dummy resource
+    def test_create_comment(self):
+        """Test that a user can create a comment."""
+        entry = {'author': 1, 'resource': 1, 'content': 'Another random post'}
+
+        # Test unsuccessful access w/out authentication
+        plain_response = self.client.post(url, entry)
+        self.assertEqual(plain_response.data, message)
+        self.assertNotEqual(plain_response.data, {})
+        self.assertEqual(plain_response.status_code, 401)
+
+        # set authentication token in header
         self.client.credentials(HTTP_AUTHORIZATION=self.token)
-        resource = {'author': 1, 'text': 'some random post'}
-        self.client.post('/api/v1/resources/', resource)
+        # Post in /api/v1/resources/1/comments/ endpoint
+        auth_response = self.client.post(url, entry)
 
-        # create a dummy comment
-        self.comment = {'author': User.objects.get().id,
-                        'content': 'finally here',
-                        'resource': Resource.objects.get().id}
+        # Asserting successful access and creation of one more resource
+        self.assertEqual(
+            auth_response.data.get('content'), 'Another random post')
+        self.assertEqual(auth_response.status_code, 201)
+        self.assertNotEqual(auth_response.data, {})
+        self.assertEqual(Comment.objects.count(), 4)
 
-        self.url2 = '/api/v1/comments/1'
-
-    def test_comment_retrieval(self):
-        """Test for comment retrieval."""
-        # unsuccessful comment retrieval without authentication.
-        self.client.credentials(HTTP_AUTHORIZATION=' ')
+    def test_get_comment(self):
+        """Test that a user can retrieve a comment."""
+        # Test unsuccessful access without authentication
         response = self.client.get(url)
         self.assertEqual(response.data, message)
-        self.assertNotEqual(response.data, {})
         self.assertEqual(response.status_code, 401)
 
-        # successful comment retrieval with authentication.
+        # Set authentication token
         self.client.credentials(HTTP_AUTHORIZATION=self.token)
+
+        # Test successful retrieval of all comments.
         auth_response = self.client.get(url)
         self.assertEqual(auth_response.status_code, 200)
+        self.assertEqual(auth_response.data['count'], 3)
 
-    def test_comment_creation(self):
-        """Test for comment creation."""
-        # unsuccessful comment creation without authentication.
-        self.client.credentials(HTTP_AUTHORIZATION=' ')
-        response = self.client.post(url)
-        self.assertEqual(response.data, message)
-        self.assertNotEqual(response.data, {})
-        self.assertEqual(response.status_code, 401)
-
-        # successful comment creation with authentication.
-        self.client.credentials(HTTP_AUTHORIZATION=self.token)
-        auth_response = self.client.post(url, self.comment)
-        self.assertEqual(auth_response.status_code, 201)
-        self.assertEqual(auth_response.data.get('content'), 'finally here')
-
-        # unsuccessful comment creation with invalid fields.
-        dummy_comment = {'author': User.objects.get().id,
-                         'content': ' ',
-                         'resource': Resource.objects.get().id}
-        response = self.client.post(url, dummy_comment)
-        self.assertEqual(response.status_code, 400)
-        self.assertIn('This field may not be blank.', response.data['content'])
-
-    def test_comment_edition(self):
-        """Test for comment update."""
-        # unsuccessful comment creation without authentication.
-        self.client.credentials(HTTP_AUTHORIZATION=' ')
-        response = self.client.put(url)
-        self.assertEqual(response.data, message)
-        self.assertNotEqual(response.data, {})
-        self.assertEqual(response.status_code, 401)
-
-        # successful comment edition with authentication.
-        new_comment = {'author': User.objects.get().id,
-                       'content': 'Django rest framework tests.',
-                       'resource': Resource.objects.get().id}
-        self.client.credentials(HTTP_AUTHORIZATION=self.token)
-        self.client.post(url, self.comment)
-        auth_response = self.client.put(self.url2, new_comment)
-        self.assertEqual(
-            auth_response.data.get('content'), 'Django rest framework tests.')
+        # Test successful retrieval of a single comment
+        auth_response = self.client.get(url2)
         self.assertEqual(auth_response.status_code, 200)
+        self.assertEqual(auth_response.data.get('content'), 'Lana Kane')
 
-        # unsuccessful comment edition with invalid fields.
-        new_comment = {'author': User.objects.get().id,
-                       'content_type': 'lala',
-                       'resource': Resource.objects.get().id}
-        self.client.credentials(HTTP_AUTHORIZATION=self.token)
-        self.client.post(url, self.comment)
-        auth_response = self.client.put(self.url2, new_comment)
-        self.assertEqual(auth_response.status_code, 400)
+        # Test that no comments are returned for a resource without comments.
+        auth_response = self.client.get(url3)
+        self.assertEqual(auth_response.status_code, 200)
+        self.assertEqual(auth_response.data['count'], 0)
 
-    def test_comment_deletion(self):
-        """Test for comment deletion."""
-        # unsuccessful comment deletion without authentication.
-        self.client.credentials(HTTP_AUTHORIZATION=' ')
-        response = self.client.delete(url)
-        self.assertEqual(response.data, message)
-        self.assertNotEqual(response.data, {})
+    def test_edit_comment(self):
+        """Test that a user can edit a comment."""
+        update = {'author': 1, 'resource': 1, 'content': 'More random post'}
+
+        # test unsuccessful update w/out authentication
+        response = self.client.put(url2, update)
         self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.data, message)
 
-        # successful comment deletion with authentication.
+        # set authentication token
         self.client.credentials(HTTP_AUTHORIZATION=self.token)
-        self.client.post(url, self.comment)
-        auth_response = self.client.delete(self.url2)
+
+        # test successful comments update
+        auth_response = self.client.put(url2, update)
+        self.assertEqual(auth_response.status_code, 200)
+        self.assertEqual(auth_response.data.get('content'), 'More random post')
+        # check that the number of comments remains constant
+        self.assertEqual(Comment.objects.count(), 3)
+
+        # test that only comments that exist can be updated
+        auth_response = self.client.put(url4, update)
+        self.assertEqual(auth_response.status_code, 404)
+        self.assertEqual(auth_response.data, not_found_msg)
+
+    def test_delete_comment(self):
+        """Test that a user can delete a comment."""
+        # test unsuccessful update w/out authentication
+        response = self.client.delete(url2)
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.data, message)
+
+        # set authentication token
+        self.client.credentials(HTTP_AUTHORIZATION=self.token)
+
+        # Test successful tcomment deletion
+        auth_response = self.client.delete(url2)
         self.assertEqual(auth_response.status_code, 204)
+        self.assertEqual(Comment.objects.count(), 2)
