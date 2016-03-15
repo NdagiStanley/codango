2585e239225f8d9c13fce6b45a633394348550f0
Stanley Ndagi <stanley.ndagi@andela.com>
2016-03-15 12:19:25 +0300

    [Feature #111595626] auth/v1/users/ and auth/v1/users/<> working

--- codango/account/api.py
+++ codango/account/api.py
@@ -3,6 +3,21 @@ from serializers import UserSerializer
 from django.contrib.auth.models import User
 
 
+class UserList(generics.ListAPIView):
+    """For /api/v1/users/ url path"""
+    permission_classes = (permissions.AllowAny,)
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class SpecificUserList(generics.RetrieveUpdateAPIView):
+    """For /api/v1/users/<id> url path"""
+    permission_classes = (permissions.AllowAny,)
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
 class UserRegisterAPIView(generics.CreateAPIView):
     """For /api/v1/auth/register url path"""
     permission_classes = (permissions.AllowAny,)
@@ -13,5 +28,6 @@ class UserRegisterAPIView(generics.CreateAPIView):
 
 class UserLogoutAPIView(generics.UpdateAPIView):
     """For /api/v1/auth/logout url path"""
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
