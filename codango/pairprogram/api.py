7b1d70b107998d98afe3fa75d62d8846014267b5
andela-jngatia <joan.ngatia@andela.com>
2016-03-16 17:40:18 +0300

    [Feature  #111595990] Create API views for /sessions/ endpoint

--- codango/pairprogram/api.py
+++ codango/pairprogram/api.py
@@ -9,10 +9,6 @@ class SessionListAPIView(generics.ListCreateAPIView):
     queryset = Session.objects.all()
     serializer_class = SessionSerializer
 
-    # def perform_create(self, serializer):
-    #     """Associate resource to an account,save data passed in request."""
-    #     serializer.save(initiator=self.request.user)
-
 
 class SessionDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     """For /api/v1/pairprogram/sessions/<session_id> url path."""
