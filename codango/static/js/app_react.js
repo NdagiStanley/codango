fd817b91b21a24cb301781668d963601a44575ea
Alex Kiura <alex.kiura@andela.com>
2016-04-20 09:47:49 +0300

    [Feature #116191173] Add about-us component to DOM

--- codango/static/js/app_react.js
+++ codango/static/js/app_react.js
@@ -1,1 +1,8 @@
 // Main entry point for react components
+var React = require('react');
+var ReactDOM = require('react-dom');
+var About = require('./components/about.jsx');
+ReactDOM.render(
+    <About />,
+    document.getElementById('about-us')
+);
