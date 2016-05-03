195f6cf0ef0ad02a5f60744a426ab2e854f6214c
Alex Kiura <alex.kiura@andela.com>
2016-05-03 10:02:19 +0300

    [Chore #116191173] Automate js builds

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 python codango/manage.py makemigrations
 python codango/manage.py migrate
 gunicorn codango.wsgi --pythonpath=codango --log-file=-
+
+webpack
+cd codango
+python manage.py collectstatic --noinput
