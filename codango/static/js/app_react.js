e9a24b8b264b9a4b801f9ace14c05060ac7430ad
Alex Kiura <alex.kiura@andela.com>
2016-05-03 10:19:07 +0300

    [Chore #116191173] Refactor js code

--- codango/static/js/app_react.js
+++ codango/static/js/app_react.js
@@ -13,7 +13,7 @@ export default class App extends Component {
         return (
             <div>
                 <Nav className="navbar navbar-default navbar-fixed-top">
-                    <div class="container">
+                    <div className="container">
                         <div className="navbar-header">
                             <Button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                                 <span className="sr-only">Toggle navigation</span>
@@ -32,19 +32,18 @@ export default class App extends Component {
                         </div>
                     </div>
                 </Nav>
-                {this.props.children}
+                { this.props.children }
             </div>
-
-
         )
     }
 }
 
 let routes = (<Router history={browserHistory}>
                 <Route path="/" component={App}>
-                  <Route path="/about" component={About}/>
-                  <Route path="/team" component={Team}/>
-                  <Route path="/contact" component={Contact}/>
+                    <Route path="/about" component={About}/>
+                    <Route path="/team" component={Team}/>
+                    <Route path="/contact" component={Contact}/>
                 </Route>
               </Router>)
+
 render(routes, document.getElementById('react'));
