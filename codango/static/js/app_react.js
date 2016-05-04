10dfb90ed942c1fe16dbd7e056e4fdd7bda41ebc
Alex Kiura <alex.kiura@andela.com>
2016-05-04 07:42:17 +0300

    [Chore #116191173] Subdivide homepage components

--- codango/static/js/app_react.js
+++ codango/static/js/app_react.js
@@ -1,45 +1,19 @@
 // Main entry point for react components
 
 import { render } from 'react-dom'
-import { Router, Route, Link, browserHistory} from 'react-router';
+import { Router, Route, Link, browserHistory, IndexRoute} from 'react-router';
 import React, {Component} from 'react';
 import About from './components/about.jsx';
+import Main from './components/main.jsx';
 import Team from './components/team.jsx';
+import Home from './components/home.jsx';
 import Contact from './components/contact.jsx';
 import {Nav, Navbar, NavbarHeader, NavItem, Button} from 'react-bootstrap';
 
-export default class App extends Component {
-    render() {
-        return (
-            <div>
-                <Nav className="navbar navbar-default navbar-fixed-top">
-                    <div className="container">
-                        <div className="navbar-header">
-                            <Button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                                <span className="sr-only">Toggle navigation</span>
-                                <span className="icon-bar"></span>
-                                <span className="icon-bar"></span>
-                                <span className="icon-bar"></span>
-                            </Button>
-                            <a className="navbar-brand" href="/"><img src="../static/img/codango-logo.png"/></a>
-                        </div>
-                        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                            <ul className="nav navbar-nav navbar-right">
-                                <li><Link to="/about">About</Link></li>
-                                <li><Link to="/team">Team</Link></li>
-                                <li><Link to="/contact">Contact us</Link></li>
-                            </ul>
-                        </div>
-                    </div>
-                </Nav>
-                { this.props.children }
-            </div>
-        )
-    }
-}
 
 let routes = (<Router history={browserHistory}>
-                <Route path="/" component={App}>
+                <Route path="/" component={Main}>
+                    <IndexRoute component={Home}/>
                     <Route path="/about" component={About}/>
                     <Route path="/team" component={Team}/>
                     <Route path="/contact" component={Contact}/>
