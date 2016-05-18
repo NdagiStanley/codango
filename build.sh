3b545ed5bb3feffe4daf049f449f266deb414688
Stanley Ndagi <stanley.ndagi@andela.com>
2016-05-18 13:13:43 +0300

    [Fix #119258129] Migrations error- add manage.py makemigrations userprofile

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 python codango/manage.py makemigrations
+python codango/manage.py makemigrations userprofile
 python codango/manage.py migrate
 gunicorn codango.wsgi --pythonpath=codango --log-file=-
