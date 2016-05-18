3184ad662497a6e081a3ae56c84257ffc5061366
Stanley Ndagi <stanley.ndagi@andela.com>
2016-05-18 13:48:20 +0300

    [Fix #119258129] Streamline migrations for all apps

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 python codango/manage.py makemigrations
-python codango/manage.py makemigrations userprofile
-python codango/manage.py makemigrations account
 python codango/manage.py migrate
 gunicorn codango.wsgi --pythonpath=codango --log-file=-
