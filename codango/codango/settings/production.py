aaa9909e9bc86fa6f0e540470116555029b65fc0
Stanley Ndagi <stanley.ndagi@andela.com>
2016-05-18 18:08:40 +0300

    [Fix #119258079] Server error (500)

--- codango/codango/settings/production.py
+++ codango/codango/settings/production.py
@@ -4,16 +4,13 @@ from .base import *
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 
-DEBUG = False
+DEBUG = True
 
 DATABASES = {
     'default': dj_database_url.config()
 }
 
 BOWER_PATH = '/app/node_modules/bower'
-
-# Enable Connection Pooling
-DATABASES['default']['ENGINE'] = 'django_postgrespool'
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
