04f3ea56e774f08e5abd2dd9c2e81231e4cc799a
Margaret Ochieng <margaret.ochieng@andela.com>
2016-05-31 18:42:44 +0300

    [Chore #120510499] Code refractor

--- codango/middleware/test_navigation.py
+++ codango/middleware/test_navigation.py
@@ -6,10 +6,7 @@ from django.contrib.auth.models import User
 class NavigationMiddleWareTest(TestCase):
     ''' Testcase for the navbar sync'''
     def setUp(self):
-        self.navbar = NavigationMiddleWare()
         self.client = Client()
-        self.request = MagicMock()
-        self.response = MagicMock()
         self.user = User.objects.create(
             id=100, username='margie', password='rain')
         self.user.set_password('rain')
@@ -20,8 +17,9 @@ class NavigationMiddleWareTest(TestCase):
         ''' Test that the active bar is highlighted'''
         response = self.client.get('/home')
         self.assertEqual(response.status_code, 200)
-        active_tab = response.context_data.get('active_tab', None)
+        active_tab = response.context_data.get('active_tab')
         self.assertIsNotNone(active_tab)
-        self.assertEqual(response.context_data['active_tab'], 'home')
+        self.assertEqual(active_tab, 'home')
         response = self.client.get('/pair/')
-        self.assertEqual(response.context_data['active_tab'], 'pair')
+        active_tab = response.context_data.get('active_tab')
+        self.assertEqual(active_tab, 'pair')
