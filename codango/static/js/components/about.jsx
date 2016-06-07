9eb2408268b29cd2dfa2b4e2691fa418b772b205
Alex Kiura <alex.kiura@andela.com>
2016-06-07 16:39:20 +0300

    [Chore] Refactor about component

--- codango/static/js/components/about.jsx
+++ codango/static/js/components/about.jsx
@@ -7,7 +7,21 @@ let Pair = require("react-material-icons/icons/action/code");
 let About = React.createClass({
   getInitialState() {
     return {
-      isFlipped: false
+      isFlipped: false,
+      aboutContent: [
+        {
+          heading: 'Community',
+          content: 'Codango is an ever expanding community of vibrant developers from different stacks and levels, helping each other grow to be even stronger developers'
+        },
+        {
+          heading: 'Sharing Resources',
+          content: 'Codango provides a platform for developers from letious stacks to upload PDFs, DOCS, code snippets and knowledgable updates making them available for all to learn and use. With endless resources available from the whole world, learning has never been easier.'
+        },
+        {
+          heading: 'Pair Programming',
+          content: 'Codango provides a platform where developers can act as mentors to other developers in real time and help grow a community of stronger developers.'
+        }
+  ]
     };
   },
   showBack() {
@@ -45,52 +59,30 @@ let About = React.createClass({
                 </div>
             </div>
           <div id="cards" className="cards">
-            {/* Default behavior is horizontal flip on hover, or focus */}
-            <FlipCard>
+            {this.state.aboutContent.map((cardInfo) => {
+                return (<Card heading={cardInfo.heading} content={cardInfo.content}/>)
+            })}
+          </div>
+        </div>
+
+    );
+  }
+});
+
+var Card = React.createClass({
+  render: function(){
+    return(
+        <FlipCard>
               {/* The first child is used as the front of the card */}
               <div>
-                <h4 className="about-h4"><strong>Community</strong></h4>
-                <div className="section-icon"><Community /></div>
-              </div>
-              {/* The second child is used as the back of the card */}
-              <div>
-                <p>Codango is an ever expanding community of vibrant developers from different stacks and levels, helping each other grow to be even stronger developers</p>
-              </div>
-            </FlipCard>
-            {/* Default behavior is horizontal flip on hover, or focus */}
-            <FlipCard>
-              {/* The first child is used as the front of the card */}
-              <div>
-                <h4 className="about-h4"><strong>Sharing Resources</strong></h4>
+                <h4 className="about-h4"><strong>{this.props.heading}</strong></h4>
                 <div className="section-icon"><Share /></div>
               </div>
               {/* The second child is used as the back of the card */}
               <div>
-                <p>Codango provides a platform for developers from letious stacks to upload PDFs, DOCS, code snippets and knowledgable updates making them available for all to learn and use. With endless resources available from the whole world, learning has never been easier.</p>
+                <p>{this.props.content}</p>
               </div>
             </FlipCard>
-            {/*
-              The `disabled` attribute allows turning off the auto-flip
-              on hover, or focus. This allows manual control over flipping.
-    ​
-              The `flipped` attribute indicates whether to show the front,
-              or the back, with `true` meaning show the back.
-            */}
-            {/* Default behavior is horizontal flip on hover, or focus */}
-            <FlipCard>
-              {/* The first child is used as the front of the card */}
-              <div>
-                <h4 className="about-h4"><strong>Pair Programming</strong></h4>
-                <div className="section-icon"><Pair /></div>
-              </div>
-              {/* The second child is used as the back of the card */}
-              <div>
-                <p>Codango provides a platform where developers can act as mentors to other developers in real time and help grow a community of stronger developers.</p>
-              </div>
-            </FlipCard>
-          </div>
-        </div>
-
     );
   }
 });
